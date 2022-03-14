497c04417ed763ae0c534c4b4c13b0609d971c2b
Oti Temitope Emmanuel <otitemitope6@gmail.com>
2022-03-14 10:13:43 +0100

    project set up

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const String appName = "NoonPool";
+const String manrope = "Manrope";
+const double kDefaultPadding = 20.0;
+const double kDefaultMargin = 20.0;
+
+const kPrimaryColor = Color.fromRGBO(145, 35, 91, 1);
+
+const kTextColor = Color.fromRGBO(55, 71, 79, 1);
+
+const String logoLocation = "assets/images/mini_logo.png";
+const String fullLogoLocation = "assets/images/logo.png";
---
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:noonpool/helpers/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  final Duration _duration = const Duration(milliseconds: 2000);
+  Animation<double>? _opacityAnimation;
+  AnimationController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    setUpAnimation();
+  }
+
+  void setUpAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: _duration,
+    );
+
+    // used to fade in the logo
+    _opacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.easeIn,
+      ),
+    );
+
+    // on fade in complete make changes to the current screen
+    _controller?.addListener(() {
+      if (_controller!.isCompleted) {
+        startTime();
+      }
+    });
+
+    // 2 seconds delay before animation starts
+    Timer(_duration, () {
+      _controller?.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  void route() {
+    // todo navigate to other screens
+  }
+
+  Future startTime() async {
+    // timer would allow screen to be navigated
+    return Timer(_duration, route);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FadeTransition(
+        opacity: _opacityAnimation!,
+        child: Center(
+          child: Image.asset(
+            fullLogoLocation,
+            height: 400,
+            width: 400,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
