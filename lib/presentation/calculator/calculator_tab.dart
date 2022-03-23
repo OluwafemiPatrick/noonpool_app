3d367d5da14fa7d10833518080069fdc4c3d37f8
Oti Temitope Emmanuel <otitemitope6@gmail.com>
2022-03-23 02:37:52 +0100

    Calculator Set Up

--- lib/presentation/calculator/calculator_tab.dart
+++ lib/presentation/calculator/calculator_tab.dart
@@ -16,7 +16,7 @@ class _CalculatorTabState extends State<CalculatorTab> {
     final textTheme = Theme.of(context).textTheme;
     final bodyText1 = textTheme.bodyText1!;
     final bodyText2 = textTheme.bodyText2!;
-    final lightText = bodyText2.copyWith(color: kLightText);
+
     const spacer = SizedBox(
       height: kDefaultMargin,
     );
@@ -27,204 +27,225 @@ class _CalculatorTabState extends State<CalculatorTab> {
         children: [
           buildAppBar(bodyText1, bodyText2),
           spacer,
-          Card(
-            elevation: 1,
-            margin: const EdgeInsets.only(
-                left: kDefaultMargin, right: kDefaultMargin),
-            color: kLightBackgroud,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(kDefaultPadding / 2),
-              child: Text(
-                'The result is the theoretical PPS+ mining yield based on the set difficulty and the average miner fees in the past 7 days',
-                style: bodyText2.copyWith(color: kPrimaryColor),
-              ),
-            ),
-          ),
+          buildTopText(bodyText2),
           spacer,
-          Padding(
-            child: Text(
-              'Difficulty',
-              style: bodyText2,
-            ),
-            padding: const EdgeInsets.only(left: kDefaultMargin),
-          ),
-          const SizedBox(
-            height: kDefaultMargin / 4,
-          ),
-          Card(
-            elevation: 1,
-            margin: const EdgeInsets.only(
-                left: kDefaultMargin, right: kDefaultMargin),
-            color: kLightBackgroud,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(kDefaultPadding / 2),
-              child: Text(
-                '21659344833264.85',
-                style: bodyText2,
-              ),
-            ),
-          ),
+          ...buildDifficulty(bodyText2),
           spacer,
-          Padding(
-            child: Text(
-              'Price',
-              style: bodyText2,
-            ),
-            padding: const EdgeInsets.only(left: kDefaultMargin),
-          ),
-          const SizedBox(
-            height: kDefaultMargin / 4,
-          ),
-          Row(
-            children: [
-              Card(
-                elevation: 1,
-                margin: const EdgeInsets.only(left: kDefaultMargin),
-                color: kLightBackgroud,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(kDefaultPadding / 2),
-                  child: Text(
-                    '63118.64',
-                    style: bodyText2,
-                  ),
-                ),
-              ),
-              const Spacer(),
-              Container(
-                margin: const EdgeInsets.only(right: kDefaultMargin),
-                padding: const EdgeInsets.all(kDefaultPadding / 4),
-                decoration: BoxDecoration(
-                    border: Border.all(width: 3, color: kLightBackgroud),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Row(
-                  children: [
-                    Text(
-                      'USD/BTC',
-                      style: bodyText2,
-                    ),
-                    const Icon(
-                      Icons.arrow_drop_down,
-                      color: kTextColor,
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+          ...buildPrice(bodyText2),
           spacer,
-          Padding(
-            child: Text(
-              'PPS Fee Rate',
-              style: bodyText2,
-            ),
-            padding: const EdgeInsets.only(left: kDefaultMargin),
-          ),
-          const SizedBox(
-            height: kDefaultMargin / 4,
-          ),
-          Card(
-            elevation: 1,
-            margin: const EdgeInsets.only(
-                left: kDefaultMargin, right: kDefaultMargin),
-            color: kLightBackgroud,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(kDefaultPadding / 2),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '4',
-                    style: bodyText2,
-                  ),
-                  const Icon(
-                    Icons.percent_rounded,
-                    color: kTextColor,
-                  )
-                ],
-              ),
-            ),
-          ),
+          ...buildPPSFeeRate(bodyText2),
           spacer,
-          Padding(
-            child: Text(
-              'Valid Hashrate',
-              style: bodyText2,
-            ),
-            padding: const EdgeInsets.only(left: kDefaultMargin),
-          ),
-          const SizedBox(
-            height: kDefaultMargin / 4,
-          ),
-          Card(
-            elevation: 1,
-            margin: const EdgeInsets.only(
-                left: kDefaultMargin, right: kDefaultMargin),
-            color: kLightBackgroud,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(kDefaultPadding / 2),
-              child: Text(
-                '1',
-                style: bodyText2,
-              ),
-            ),
-          ),
+          ...buildValidHashRate(bodyText2),
           spacer,
           const Divider(),
           spacer,
-          Padding(
-            child: Text(
-              'EST. Daily Yield',
-              style: bodyText2,
-            ),
-            padding: const EdgeInsets.only(
-                left: kDefaultPadding, right: kDefaultPadding),
-          ),
-          const SizedBox(
-            height: kDefaultMargin / 4,
-          ),
-          Padding(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text(
-                  '0.00000565',
-                  style: bodyText1.copyWith(fontSize: 25),
-                ),
-                const SizedBox(
-                  width: kDefaultMargin / 5,
-                ),
-                Text(
-                  'BTC',
-                  style: bodyText2,
-                ),
-                const Spacer(),
-                Text(
-                  '= \$ 0.3566',
-                  style: bodyText2.copyWith(color: kPrimaryColor),
-                ),
-              ],
-            ),
-            padding: const EdgeInsets.only(
-                left: kDefaultPadding, right: kDefaultPadding),
-          ),
+          ...buildTotal(bodyText2, bodyText1),
         ],
       ),
     );
+  }
+
+  Card buildTopText(TextStyle bodyText2) {
+    return Card(
+      margin:
+          const EdgeInsets.only(left: kDefaultMargin, right: kDefaultMargin),
+      color: kLightBackgroud,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: Text(
+          'The result is the theoretical PPS+ mining yield based on the set difficulty and the average miner fees in the past 7 days',
+          style: bodyText2.copyWith(color: kPrimaryColor),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> buildDifficulty(TextStyle bodyText2) {
+    return [
+      Padding(
+        child: Text(
+          'Difficulty',
+          style: bodyText2,
+        ),
+        padding: const EdgeInsets.only(left: kDefaultMargin),
+      ),
+      const SizedBox(
+        height: kDefaultMargin / 4,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: kLightBackgroud,
+        ),
+        margin:
+            const EdgeInsets.only(left: kDefaultMargin, right: kDefaultMargin),
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: Text(
+          '21659344833264.85',
+          style: bodyText2,
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> buildPrice(TextStyle bodyText2) {
+    return [
+      Padding(
+        child: Text(
+          'Price',
+          style: bodyText2,
+        ),
+        padding: const EdgeInsets.only(left: kDefaultMargin),
+      ),
+      const SizedBox(
+        height: kDefaultMargin / 4,
+      ),
+      Row(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: kLightBackgroud,
+            ),
+            margin: const EdgeInsets.only(left: kDefaultMargin),
+            padding: const EdgeInsets.all(kDefaultPadding / 2),
+            child: Text(
+              '63118.64',
+              style: bodyText2,
+            ),
+          ),
+          const Spacer(),
+          Container(
+            margin: const EdgeInsets.only(right: kDefaultMargin),
+            padding: const EdgeInsets.all(kDefaultPadding / 4),
+            decoration: BoxDecoration(
+                border: Border.all(width: 3, color: kLightBackgroud),
+                borderRadius: BorderRadius.circular(10)),
+            child: Row(
+              children: [
+                Text(
+                  'USD/BTC',
+                  style: bodyText2,
+                ),
+                const Icon(
+                  Icons.arrow_drop_down,
+                  color: kTextColor,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    ];
+  }
+
+  List<Widget> buildPPSFeeRate(TextStyle bodyText2) {
+    return [
+      Padding(
+        child: Text(
+          'PPS Fee Rate',
+          style: bodyText2,
+        ),
+        padding: const EdgeInsets.only(left: kDefaultMargin),
+      ),
+      const SizedBox(
+        height: kDefaultMargin / 4,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: kLightBackgroud,
+        ),
+        margin:
+            const EdgeInsets.only(left: kDefaultMargin, right: kDefaultMargin),
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '4',
+              style: bodyText2,
+            ),
+            const Icon(
+              Icons.percent_rounded,
+              color: kTextColor,
+            )
+          ],
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> buildValidHashRate(TextStyle bodyText2) {
+    return [
+      Padding(
+        child: Text(
+          'Valid HashRate',
+          style: bodyText2,
+        ),
+        padding: const EdgeInsets.only(left: kDefaultMargin),
+      ),
+      const SizedBox(
+        height: kDefaultMargin / 4,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: kLightBackgroud,
+        ),
+        margin:
+            const EdgeInsets.only(left: kDefaultMargin, right: kDefaultMargin),
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: Text(
+          '1',
+          style: bodyText2,
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> buildTotal(TextStyle bodyText2, TextStyle bodyText1) {
+    return [
+      Padding(
+        child: Text(
+          'EST. Daily Yield',
+          style: bodyText2,
+        ),
+        padding: const EdgeInsets.only(
+            left: kDefaultPadding, right: kDefaultPadding),
+      ),
+      const SizedBox(
+        height: kDefaultMargin / 4,
+      ),
+      Padding(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              '0.00000565',
+              style: bodyText1.copyWith(fontSize: 25),
+            ),
+            const SizedBox(
+              width: kDefaultMargin / 5,
+            ),
+            Text(
+              'BTC',
+              style: bodyText2,
+            ),
+            const Spacer(),
+            Text(
+              '= \$ 0.3566',
+              style: bodyText2.copyWith(color: kPrimaryColor),
+            ),
+          ],
+        ),
+        padding: const EdgeInsets.only(
+            left: kDefaultPadding, right: kDefaultPadding),
+      ),
+    ];
   }
 
   AppBar buildAppBar(TextStyle? bodyText1, TextStyle bodyText2) {
